f6fd20f5d32ef5d89c5e368839fe4d4302257f77
Pedro Padilha <pedropadilha@me.com>
2018-10-26 00:12:01 -0300

    Added nodemon and watch script

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -23,12 +25,12 @@ app.use('/api', apiRouter);
 app.use('/', viewRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
